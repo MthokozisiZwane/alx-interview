d1631bfcc7d7782fadff7dc9fe972b952a0d2463
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2024-03-10 11:58:53 -0700

    Pascal Triangle generation

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,24 @@
+def pascal_triangle(n):
+    """
+    A function that generates pascal triangle to given n rows
+    
+    args:
+        n: number of rows to generate pascal triangle
+    Return: A list of lists representing pascal triangle
+    """
+    
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
+
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
