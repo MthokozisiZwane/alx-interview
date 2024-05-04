99dd3dca3a43101c6da3fbd33255912805a40a44
MthokozisiZwane <mhokozisi.zwane1999@gmail.com>
2024-05-04 01:30:25 +0000

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,7 +2,6 @@
 """
 Module for making change.
 """
-import math
 
 
 def makeChange(coins, total):
@@ -20,13 +19,15 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    min_coins_n = [math.inf] * (total + 1)
-    min_coins_n[0] = 0
+    coins.sort(reverse=True)
 
-    for amount in range(1, total + 1):
-        for coin in coins:
-            if coin <= amount:
-                min_coins_n[amount] = min(min_coins_n[amount],
-                                          min_coins_n[amount - coin] + 1)
+    min_coins_needed = 0
+    remaining_amount = total
 
-    return min_coins_n[total] if min_coins_n[total] != math.inf else -1
+    for coin in coins:
+        if remaining_amount >= coin:
+            num_coins = remaining_amount // coin
+            min_coins_needed += num_coins
+            remaining_amount -= num_coins * coin
+
+    return min_coins_needed if remaining_amount == 0 else -1
