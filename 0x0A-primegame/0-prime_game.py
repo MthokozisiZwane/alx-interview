956f643c3518bf69ca20b0950dea35dc1ddceafc
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2024-05-20 10:33:03 +0000

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,60 +1,53 @@
 #!/usr/bin/python3
-"""
-Dtermines the winner in a game where prime numbers along with their
-multiples are removed from set one player at a time.
-"""
+
+
+def sieve_of_eratosthenes(max_n):
+    """ Generates a list of primes up to max_n using the
+    Sieve of Eratosthenes
+    """
+    is_prime = [True] * (max_n + 1)
+    is_prime[0], is_prime[1] = False, False
+    p = 2
+    while p * p <= max_n:
+        if is_prime[p]:
+            for multiple in range(p * p, max_n + 1, p):
+                is_prime[multiple] = False
+        p += 1
+    primes = [num for num, prime in enumerate(is_prime) if prime]
+    return primes, is_prime
+
+
+def calculate_winning_positions(max_n, primes, is_prime):
+    """ Calculate winning positions for each number of primes up to max_n """
+    winning = [False] * (max_n + 1)
+    for num in range(2, max_n + 1):
+        for prime in primes:
+            if prime > num:
+                break
+            if not winning[num - prime]:
+                winning[num] = True
+                break
+    return winning
 
 
 def isWinner(x, nums):
-    """ function to determine winner"""
-    def sieve_of_eratosthenes(n):
-        """ function to loop through prime numbers"""
-        prime_numb = [True] * (n + 1)
-        prime_numb[0] = prime_numb[1] = False
-        p = 2
-        while p ** 2 <= n:
-            if prime_numb[p]:
-                for i in range(p ** 2, n + 1, p):
-                    prime_numb[i] = False
-            p += 1
-        return [p for p in range(2, n + 1) if prime_numb[p]]
+    """ count player wins"""
+    max_n = max(nums)
+    primes, is_prime = sieve_of_eratosthenes(max_n)
+    winning = calculate_winning_positions(max_n, primes, is_prime)
 
-    def is_prime(n):
-        """ determines if number is prime
-        """
-        if n < 2:
-            return False
-        for p in range(2, int(n ** 0.5) + 1):
-            if n % p == 0:
-                return False
-        return True
+    maria_wins = 0
+    ben_wins = 0
 
-    def play_game(n):
-        """
-        function to play the game
-        """
-        prime_numb = sieve_of_eratosthenes(n)
-        remaining = set(range(1, n + 1))
-        winner = None
-        while remaining:
-            if not any(is_prime(num) for num in remaining):
-                break
-            if winner == 'Maria':
-                winner = 'Ben'
-            else:
-                winner = 'Maria'
-            prime = next((num for num in prime_numb if num in remaining), None)
-            remaining -= set(range(prime, n + 1, prime))
-        return winner
-
-    wins = {'Maria': 0, 'Ben': 0}
     for n in nums:
-        winner = play_game(n)
-        if winner:
-            wins[winner] += 1
-    if wins['Maria'] > wins['Ben']:
-        return 'Maria'
-    elif wins['Ben'] > wins['Maria']:
-        return 'Ben'
+        if winning[n]:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
     else:
         return None
